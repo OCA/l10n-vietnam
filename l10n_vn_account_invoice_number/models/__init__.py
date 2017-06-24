f2186e21ebd3b9bece68aeb286e90f74bdbd3323
HaiDD <haidd.uit@gmail.com>
2017-06-24 10:26:55 +0700

    [REF] Add license to .py file

--- l10n_vn_account_invoice_number/models/__init__.py
+++ l10n_vn_account_invoice_number/models/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
+# © 2006-2017 Trobz (http://trobz.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 import account_invoice
 import account_voucher
--- l10n_vn_account_invoice_number/models/account_voucher.py
+++ l10n_vn_account_invoice_number/models/account_voucher.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# © 2006-2017 Trobz (http://trobz.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import fields, models
 
 
