2ad3ac446f9d7811d37c976f42b805e48a65154e
Fanha Giang <fanha99@hotmail.com>
2017-08-23 01:08:24 +0700

    hide if the owner of invoice is not in vietnam

--- l10n_vn_account_invoice_number/models/account_invoice.py
+++ l10n_vn_account_invoice_number/models/account_invoice.py
@@ -12,18 +12,37 @@ class AccountInvoice(models.Model):
     tax_invoice_number = fields.Integer()
     tax_invoice_form = fields.Char()
     tax_invoice_serie = fields.Char()
+    is_owner_in_vietnam = fields.Boolean(string='Is Owner in Vietnam', store=False, compute='_is_owner_in_vietnam')
+
+    @api.onchange('partner_id', 'company_id')
+    def _onchange_partner_id(self):
+        self._is_owner_in_vietnam()
+        return super(AccountInvoice, self)._onchange_partner_id()
+
+    @api.one
+    def _is_owner_in_vietnam(self):
+        invoice_owner_country_code = ''
+        if self.type == 'out_invoice' or self.type == 'in_refund':
+            invoice_owner_country_code = self.env.user.company_id.partner_id.country_id.code
+        else:
+            invoice_owner_country_code = self.partner_id.country_id.code
+        if invoice_owner_country_code == 'VN':
+            self.is_owner_in_vietnam = True
+        else:
+            self.is_owner_in_vietnam = False
 
     @api.multi
     def action_invoice_open(self):
         # Check invoice number, form, serie before validating
-        for inv in self:
-            if not inv.invoice_number:
-                raise UserError(
-                    _("Missing invoice number"))
-            if not inv.invoice_form:
-                raise UserError(
-                    _("Missing invoice form"))
-            if not inv.invoice_serie:
-                raise UserError(
-                    _("Missing invoice serie"))
+        if self.is_owner_in_vietnam:
+            for inv in self:
+                if not inv.tax_invoice_number:
+                    raise UserError(
+                        _("Missing tax invoice number"))
+                if not inv.tax_invoice_form:
+                    raise UserError(
+                        _("Missing tax invoice form"))
+                if not inv.tax_invoice_serie:
+                    raise UserError(
+                        _("Missing tax invoice serie"))
         return super(AccountInvoice, self).action_invoice_open()
