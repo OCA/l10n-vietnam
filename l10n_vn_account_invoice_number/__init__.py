afc12546422ee8030d2dcf5f8a1e5f5a47717e0f
Fanha Giang <fanha99@hotmail.com>
2017-08-31 15:26:06 +0700

    from haidd

---
+++ l10n_vn_account_invoice_number/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2006-2017 Trobz (http://trobz.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
---
+++ l10n_vn_account_invoice_number/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2006-2017 Trobz (http://trobz.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import account_invoice
+from . import account_voucher
