d28d53e049060655ad630dea58b1a67da008b696
Fanha Giang <fanha99@hotmail.com>
2017-11-13 12:20:56 +0700

    redundant imports: api, _

--- l10n_vn_account_invoice_number/models/res_company.py
+++ l10n_vn_account_invoice_number/models/res_company.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Trobz (http://trobz.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
