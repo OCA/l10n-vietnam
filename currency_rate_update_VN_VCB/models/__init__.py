8dca1fe8dafc21d03633a491b13b1b55f69c25f7
denislour <denislour@gmail.com>
2018-10-18 18:09:06 +0700

    Add the Komit copyright.

--- currency_rate_update_VN_VCB/models/__init__.py
+++ currency_rate_update_VN_VCB/models/__init__.py
@@ -1,1 +1,4 @@
+# © 2018 Komit <http://komit-consulting.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import currency_rate_update
--- currency_rate_update_VN_VCB/models/currency_rate_update.py
+++ currency_rate_update_VN_VCB/models/currency_rate_update.py
@@ -1,3 +1,6 @@
+# © 2018 Komit <http://komit-consulting.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import api, fields, models, _
 
 
