e8499edfe07fffb14c420a91ba1f54b6931fc211
denislour <denislour@gmail.com>
2018-10-18 17:59:23 +0700

    Force inverted the currency supported.

--- currency_rate_update_VN_VCB/__init__.py
+++ currency_rate_update_VN_VCB/__init__.py
@@ -1,4 +1,5 @@
 # © 2018 Komit <http://komit-consulting.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import models
 from . import services
---
+++ currency_rate_update_VN_VCB/models/currency_rate_update.py
@@ -0,0 +1,23 @@
+from odoo import api, fields, models, _
+
+
+class CurrencyRateUpdateService(models.Model):
+    _inherit = "currency.rate.update.service"
+
+    @api.multi
+    def write(self, vals):
+        self.fore_inverted_currency(self.service, vals)
+        return super(CurrencyRateUpdateService, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        self.fore_inverted_currency(vals.get('service'), vals)
+        return super(CurrencyRateUpdateService, self).create(vals)
+
+    def fore_inverted_currency(self, service, vals):
+        if service == 'VN_VCB':
+            for currency_id in vals.get('currency_to_update')[0][2]:
+                currency = self.env['res.currency'].browse(currency_id)
+                if currency.name == 'VND':
+                    continue
+                currency.write({'rate_inverted': True})
