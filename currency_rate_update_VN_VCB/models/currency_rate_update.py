e62cb44bc9cd8cb5cb9ecf4d46e272d815d13b0f
denislour <denislour@gmail.com>
2018-10-19 10:06:07 +0700

    Fix pyflake8

--- currency_rate_update_VN_VCB/models/currency_rate_update.py
+++ currency_rate_update_VN_VCB/models/currency_rate_update.py
@@ -1,7 +1,7 @@
 # © 2018 Komit <http://komit-consulting.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import api, models
 
 
 class CurrencyRateUpdateService(models.Model):
